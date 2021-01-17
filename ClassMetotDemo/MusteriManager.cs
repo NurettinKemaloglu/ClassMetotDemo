3b8b55a8568e921f9eaa9b2dbf58d9a9f64feca4
nurettinkemaloglu25@gmail.com <nurettinkemaloglu25@gmail.com>
2021-01-17 16:29:25 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void EkleMusteri (Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi+":"+ musteri.MusteriSoyadi+":"+musteri.Sehir+":"+musteri.MusteriTel+"Müşteri Sisteme Eklendi");
+        }
+        public void SilMusteri (Musteri musteri)
+        {
+            Console.WriteLine(musteri.MusteriAdi + ":" + musteri.MusteriSoyadi + ":" + musteri.Sehir + ":" + musteri.MusteriTel + "Müşteri Sistemden Silindi ");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.MusteriAdi = "MERVE";
+            musteri1.MusteriSoyadi = "güzel";
+            musteri1.MusteriTel = 05303802222;
+            musteri1.Sehir = "Adana";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.MusteriAdi = "Nurettin";
+            musteri2.MusteriSoyadi = "KEMALOĞLU";
+            musteri2.MusteriTel = 05555555555;
+            musteri2.Sehir = "Erzurum";
+
+            Musteri[] musteriler = new Musteri[]
+            { 
+                musteri1 ,musteri2
+            };
+
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.MusteriAdi);
+                Console.WriteLine(musteri.MusteriSoyadi);
+                Console.WriteLine(musteri.MusteriTel);
+                Console.WriteLine(musteri.Sehir);
+                Console.WriteLine("++++++++++++++++++++++++++++++++++");
+            }
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.EkleMusteri(musteri1);
+            musteriManager.SilMusteri(musteri2);
+        }
+    }
+}
